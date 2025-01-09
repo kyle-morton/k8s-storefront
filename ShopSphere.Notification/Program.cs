d9076c7d5ad970ff49e4c48b91e55c7acdc537be
Kyle Morton <mkmorton91@gmail.com>
2025-01-08 22:04:12 -0600

    Working on appSettings issues w/ pushing to K8S

--- ShopSphere.Notification/Program.cs
+++ ShopSphere.Notification/Program.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShopSphere.Notification.Clients.Models;
 using ShopSphere.Notification.Events;
 
-var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-Console.WriteLine($"Environment: {environmentName}");
+// Run locally in Development -> dotnet run --environment Development
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+Console.WriteLine($"Environment: {environmentName}");
+Console.WriteLine($"CurrentDir: {Environment.CurrentDirectory}");
+
+builder.Configuration
+    .SetBasePath(Environment.CurrentDirectory)
+    .AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables();
 
 // Register Automapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
